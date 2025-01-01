e9924f2b158057c06df0ca47930f815addbf9f31
Viktorijagit <viki.kozulovic14@gmail.com>
2025-01-01 14:33:46 +0100

    Add files via upload
    Dodavanje HelloWorldviki programa

---
+++ moj pyhton program.py
@@ -0,0 +1,42 @@
+Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+for _ in range(36):
+...     print("Hello World")
+... 
+...     
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
+Hello World
